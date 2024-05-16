ce41a0f8fca92f91b39d41e33681a33c5550f7d9
Torniketreader123 <128076687+Torniketreader123@users.noreply.github.com>
2024-05-16 23:44:41 +0400

    ATM_1

---
+++ src/screens/HomeScreen.tsx
@@ -0,0 +1,18 @@
+import * as React from 'react';
+import { Text, View, StyleSheet } from 'react-native';
+
+
+
+const HomeScreen = () => {
+  return (
+    <View style={styles.container}>
+      <Text>componentName</Text>
+    </View>
+  );
+};
+
+export default HomeScreen;
+
+const styles = StyleSheet.create({
+  container: {}
+});
