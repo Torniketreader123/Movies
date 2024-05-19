5986ebfb7858469ea0e6d3c14977bc5f064a04bc
Torniketreader123 <128076687+Torniketreader123@users.noreply.github.com>
2024-05-19 13:57:44 +0400

    ATM_6

--- src/screens/MovieDetailsScreen.tsx
+++ src/screens/MovieDetailsScreen.tsx
@@ -1,7 +1,42 @@
 import { StyleSheet, Text, View } from 'react-native'
-import React from 'react'
+import React, { useEffect, useState } from 'react'
+import { movieCastDetails, movieDetails } from '../api/apicalls'
 
-const MovieDetailsScreen = () => {
+const getMovieDetails = async (movieid :number)=>{
+  try{
+    let response = await fetch(movieDetails(movieid))
+    let json = await response.json();
+    return json;
+}catch{
+  console.log('something wrong in getmovoies funqtion ')
+}
+}
+
+
+const getMovieCastDetails = async (movieid :number)=>{
+  try{
+    let response = await fetch(movieCastDetails(movieid))
+    let json = await response.json();
+    return json;
+}catch{
+  console.log('something wrong in getcastmovoies funqtion ')
+}
+}
+
+const MovieDetailsScreen = ({navigation,route}:any) => {
+  const [movieData, setMoviedata]= useState(undefined);
+  const [movieCastDat, setMovieCastData]= useState<any>(undefined);
+  useEffect(()=>{
+    (async () => {
+      const  tempMovieData = await getMovieDetails(route.params.movievd);
+      setMoviedata(tempMovieData)
+    })();
+    (async () => {
+      const  tempMovieData = await getMovieDetails(route.params.movievd);
+      setMoviedata(tempMovieData)
+    })();
+
+  },[])
   return (
     <View>
       <Text>MovieDetailsScreen</Text>
